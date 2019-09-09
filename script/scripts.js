37891a8ea5e33ac5fc261092423ca31f164fa5ca
Roy <roy.felix1609@gmail.com>
2019-09-09 15:22:54 +1000

    Test

--- script/scripts.js
+++ script/scripts.js
@@ -3,7 +3,7 @@ $('document').ready(function() {
     const URL = "http://ec2-3-14-6-238.us-east-2.compute.amazonaws.com:3000";
 
         $.ajax({
-            url: URL + "/api/menus/get/menuByType/3",
+            url: URL + "/api/menus/get/all",
             type: "GET",
             success: function(result) {
                 console.log(result.data);
