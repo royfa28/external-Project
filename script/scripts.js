f3aceb3b7aec3740c0acbb0e367fba81f7708ae9
Roy <roy.felix1609@gmail.com>
2019-09-09 14:30:28 +1000

    order page popup

--- script/scripts.js
+++ script/scripts.js
@@ -11,11 +11,29 @@ $('document').ready(function() {
                 // show product here. You can do the table but I will show only simple way.
                 for (var i = 0; i < result.data.length; i++) {
 
-                    temp += '<div class="card-deck" style="float:left; width:25%; padding:5px; height:400px;">'
-                    + '<div class="card"> <img class="card-img-top" style="width:200px; margin: 0 auto;"; src="images/' + result.data[i].pictureName + '"> <div class="card-body">'
-                    + '<h5 class="card-title" style="font-weight:bold;">' + result.data[i].name + '</h5>'
-                    + '<p class="card-text">' + result.data[i].description + '</p> </div>'
-                    + '<div class="card-footer"> <small class="text-muted"> Add to Cart </small> </div> </div> </div>';
+            temp += '<div class="card-deck" style="float:left; width:25%; padding:5px; height:400px;">'
+                    + '<div class="card">'
+                        + '<img class="card-img-top" style="width:200px; margin: 0 auto;"; src="images/' + result.data[i].pictureName + '">'
+                            + '<div class="card-body">'
+                            + '<h5 class="card-title" style="font-weight:bold;">' + result.data[i].name + '</h5>'
+                            + '<p class="card-text">' + result.data[i].description + '</p> </div>'
+                                + '<div class="card-footer">'
+                                    + '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter"> Add to Cart </button>'
+                                    + '</div> </div> </div>'
+                    
+                    + '<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">'
+                    + '<div class="modal-dialog modal-dialog-centered" role="document">'
+                        + '<div class="modal-content">'
+                        + '<div class="modal-header">'
+                            + '<h5 class="modal-title" style="font-weight:bold; id="exampleModalCenterTitle">' + result.data[i].name +'</h5>'
+                            + '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'
+                            + '<span aria-hidden="true">&times;</span> </button>'
+                        + '</div>'
+                        + '<div class="modal-body">...</div>'
+                        + '<div class="modal-footer">'
+                            + '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'
+                            + '<button type="button" class="btn btn-primary">Save changes</button>'
+                        +'</div> </div> </div> </div>';
 
                     
                     /*
@@ -37,3 +55,7 @@ $('document').ready(function() {
         })
     
 });
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').modal('focus')
+  });
