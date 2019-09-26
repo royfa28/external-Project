1dd528733edd5e9d4ea2e933073a1a5b5855552a
Roy <roy.felix1609@gmail.com>
2019-09-27 08:36:43 +1000

    shopcart

---
+++ shopcart.php
@@ -0,0 +1,27 @@
+<?php
+require('vendor/autoload.php');
+
+// Test for navigation after auth
+/*
+session_start();
+$_SESSION['auth'] = true;
+session_destroy();
+*/
+
+use aitSydney\Navigation;
+
+//create twig loader
+//$loader = new \Twig\Loader\FilesystemLoader('templates');
+$loader = new Twig_Loader_Filesystem('templates');
+
+//create twig environment
+$twig = new Twig_Environment($loader);
+
+//load a twig template
+$template = $twig -> load('shopcart.twig');
+
+//pass values to twig
+echo $template -> render([
+    'title' => 'Hello shop'
+]);
+?>
