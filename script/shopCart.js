85862fa0b283db91e5ce5aed236ef11e208ce93a
itsupportnsw <itsupport@nswenglish.com.au>
2019-09-27 15:40:50 +1000

    test edit

--- script/shopCart.js
+++ script/shopCart.js
@@ -93,3 +93,6 @@ function updateCartTotal() {
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
+
+
+TextDecoderStream
